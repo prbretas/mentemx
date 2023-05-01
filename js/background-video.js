3667375b233e17a03fa8f32e1f7c09f048828161
Philippe R. Bretas <86687733+prbretas@users.noreply.github.com>
2023-05-01 16:31:50 -0300

    update

---
+++ js/background-video.js
@@ -0,0 +1,50 @@
+let sourceVideos = [];
+sourceVideos[0] = "https://vod-progressive.akamaized.net/exp=1682631556~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F203%2F19%2F476017480%2F2124585976.mp4~hmac=ac51e5dc9d20721e2f03290165745fb5aaec54a5320319cbb44786f4aefc8d42/vimeo-prod-skyfire-std-us/01/203/19/476017480/2124585976.mp4";
+sourceVideos[1] = "https://vod-progressive.akamaized.net/exp=1682631513~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2738%2F8%2F213694565%2F735343305.mp4~hmac=603a09b3321d682872d16db32c1436f1c06d512c6f5603c2095795cc678f72c6/vimeo-prod-skyfire-std-us/01/2738/8/213694565/735343305.mp4";
+sourceVideos[2] = "https://vod-progressive.akamaized.net/exp=1682631607~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F203%2F19%2F476017079%2F2124584658.mp4~hmac=f5ead47b8849e2b44325ff75b7143e0115c408219bf7309039aa1b202b6acfce/vimeo-prod-skyfire-std-us/01/203/19/476017079/2124584658.mp4";
+sourceVideos[3] = "https://vod-progressive.akamaized.net/exp=1682631635~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F210%2F19%2F476052734%2F2124750155.mp4~hmac=307e23b0a57adacf3eb55c239c1a2688ac941b68722dc1912deb21b35666361d/vimeo-prod-skyfire-std-us/01/210/19/476052734/2124750155.mp4";
+sourceVideos[4] = "https://vod-progressive.akamaized.net/exp=1682631653~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F203%2F19%2F476017006%2F2124583986.mp4~hmac=70a8b4665a4d26a784fcf3e3359bcdc5bdf43bfaf90dbcdd8b8305f55c1a248e/vimeo-prod-skyfire-std-us/01/203/19/476017006/2124583986.mp4";
+sourceVideos[5] = "https://vod-progressive.akamaized.net/exp=1682631659~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F203%2F19%2F476015872%2F2124582428.mp4~hmac=2fcfa4e974f0edc51e0aece02590d45fd2ff9465ab7cf4a5369103587eff4cbb/vimeo-prod-skyfire-std-us/01/203/19/476015872/2124582428.mp4";
+sourceVideos[6] = "https://vod-progressive.akamaized.net/exp=1682631671~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F209%2F19%2F476047264%2F2124718727.mp4~hmac=8f6ff1f2ec1ca85a9a9836dbaaadb58d0e90af2596f6f82cd9da981ce5b52714/vimeo-prod-skyfire-std-us/01/209/19/476047264/2124718727.mp4";
+sourceVideos[7] = "https://vod-progressive.akamaized.net/exp=1682631734~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F210%2F19%2F476052544%2F2124749286.mp4~hmac=03433e286264149b20bf85f3bf8fe616a9f50bd814d719d974348a4aeb5daa97/vimeo-prod-skyfire-std-us/01/210/19/476052544/2124749286.mp4";
+sourceVideos[8] = "https://vod-progressive.akamaized.net/exp=1682631739~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F210%2F19%2F476052474%2F2124748926.mp4~hmac=27dc9d04e5d4fb7117623628f4a40dfbba14dc3e3f00a06eb7c5c4a0ddb689f2/vimeo-prod-skyfire-std-us/01/210/19/476052474/2124748926.mp4";
+sourceVideos[9] = "https://vod-progressive.akamaized.net/exp=1682631783~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3127%2F26%2F665637255%2F3062535146.mp4~hmac=f636271402cf13c73759d23bca90d9c02cc9ec22db7e4bd53135904eb4f298b6/vimeo-prod-skyfire-std-us/01/3127/26/665637255/3062535146.mp4?filename=360p.mp4";
+sourceVideos[10] = "https://vod-progressive.akamaized.net/exp=1682631790~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2465%2F23%2F587328277%2F2771271474.mp4~hmac=76b40c6e7ed9908d3b9f60600007fdd808e1a2eb6d6858eb1ec6b336a0ed702a/vimeo-prod-skyfire-std-us/01/2465/23/587328277/2771271474.mp4";
+sourceVideos[11] = "https://vod-progressive.akamaized.net/exp=1682631795~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F210%2F19%2F476053022%2F2124751616.mp4~hmac=9a258e4afc3cf01468cf049c0dcf5793634a9c68d6ff7fd0cf98b7f4bb9a9974/vimeo-prod-skyfire-std-us/01/210/19/476053022/2124751616.mp4";
+
+sourceVideos[12] = "https://joy1.videvo.net/videvo_files/video/free/2014-12/large_watermarked/Raindrops_Videvo_preview.mp4";
+sourceVideos[13] = "";
+sourceVideos[14] = "https://joy1.videvo.net/videvo_files/video/free/2021-04/large_watermarked/210329_01B_Bali_1080p_009_preview.mp4";
+sourceVideos[15] = "https://joy1.videvo.net/videvo_files/video/free/2019-05/large_watermarked/190516_06_AZ-LAGOA-30_preview.mp4";
+sourceVideos[16] = "https://joy1.videvo.net/videvo_files/video/free/2014-07/large_watermarked/Galaxy_With_Customization_preview.mp4";
+sourceVideos[17] = "https://joy1.videvo.net/videvo_files/video/free/2016-05/large_watermarked/506401051_1_preview.mp4";
+sourceVideos[18] = "https://joy1.videvo.net/videvo_files/video/free/2016-11/large_watermarked/Smoke_Dark_11_Videvo_preview.mp4";
+sourceVideos[19] = "https://video.motionplaces.com/motionplaces.com-0025-c014.mp4";
+sourceVideos[20] = "";
+sourceVideos[21] = "https://video.motionplaces.com/motionplaces.com-0048-c006.mp4";
+sourceVideos[22] = "https://video.motionplaces.com/motionplaces.com-0031-c019.mp4";
+sourceVideos[23] = "https://video.motionplaces.com/motionplaces.com-0049-c016.mp4";
+sourceVideos[24] = "https://video.motionplaces.com/motionplaces.com-0032-c034.mp4";
+sourceVideos[25] = "https://video.motionplaces.com/motionplaces.com-0028-c041.mp4";
+sourceVideos[26] = "";
+sourceVideos[27] = "https://video.motionplaces.com/motionplaces.com-0017-c026.mp4";
+sourceVideos[28] = "";
+sourceVideos[29] = "";
+sourceVideos[30] = "https://joy1.videvo.net/videvo_files/video/free/2017-01/large_watermarked/timelapsed_oviedo_preview.mp4";
+
+//GERAR NUMERO ALEATORIO ENTRE MINIMO E MAXIMO
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+//CHAMANDO A FUNCAO E ATRIBUINDO A UMA VARIAVEL
+let numeroAleatorio = getRandomInt(0,30)
+
+//ALTERANDO O ATRIBUTO SRC DO ELEMENTO HTML VIDEO
+let videoBackground = document.getElementById("videoBackground")
+videoBackground.setAttribute('src', sourceVideos[numeroAleatorio])
+
+//MOSTRAR NO CONSOLE NUMERO ALEATORIO GERADO
+console.log("O número do video selecionado é " + numeroAleatorio)
